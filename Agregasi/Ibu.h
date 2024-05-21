5de82d92097122b261b31c9f82d7e8c067f44fde
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-21 19:24:57 +0700

    void ibu cetak anak

--- Agregasi/Ibu.h
+++ Agregasi/Ibu.h
@@ -23,3 +23,11 @@ void ibu::tambahAnak(anak* pAnak)
 {
 	daftar_anak.push_back(pAnak);
 }
+void ibu::cetakAnak()
+{
+	cout << "Daftar Anak dari ibu \"" << this->nama << "\":\n";
+	/*for (auto& a : daftar_anak)
+	{
+	cout << a->nama << "\n";
+	}*/
+}

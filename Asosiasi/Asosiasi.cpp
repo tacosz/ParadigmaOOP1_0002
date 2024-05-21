1e0de6a8564a49d577abb45268e072c7eb86c1a3
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-21 20:14:46 +0700

    void dokter cetak pasien dan perulangan for

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -51,6 +51,20 @@ void pasien::cetakDokter()
     }
     cout << endl;
 }
+void dokter::tambahPasien(pasien* pPasien)
+{
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+void dokter::cetakPasien()
+{
+    cout << "Daftar Pasoen dari dokter \"" << this->nama << "\":\n";
+    for (auto& a : daftar_pasien)
+    {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
 int main()
 {
     

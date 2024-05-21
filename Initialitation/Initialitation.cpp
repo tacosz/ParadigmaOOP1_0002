a781bce2d886b3e902084424a938a64371bed50d
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-21 17:35:18 +0700

    Add project files.

---
+++ Initialitation/Initialitation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+private:
+    const int id;
+    string nama;
+    float nilai;
+
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}

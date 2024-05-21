be7c3e5be2764e4ab87ff67582be7e14aab1442c
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-21 19:51:53 +0700

    konstruktor pasien dalam asosiasi

---
+++ Asosiasi/Asosiasi.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class dokter;
+class pasien //asosiasi
+{
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama)
+    {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+};
+
+int main()
+{
+    
+}

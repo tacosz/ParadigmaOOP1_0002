3aa64722cfc6dbd996b4969e53dd3e59bbf440c2
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-21 18:27:58 +0700

    constructor dan destructor dalam header manusia

--- Composition/Manusia.h
+++ Composition/Manusia.h
@@ -9,4 +9,14 @@ public:
 	string name;
 	jantung varJantung;
 
+	manusia(string pName)
+		: name(pName)
+	{
+		cout << name << " hidup\n";
+	}
+	~manusia()
+	{
+		cout << name << " mati\n";
+	}
 };
+#endif
